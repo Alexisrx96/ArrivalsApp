f6289f0c8fcba6fb9d920ce27562b436260f2acb
Irvin Torres <irvinrx1996@hotmail.com>
2025-01-12 17:16:01 -0600

    Add models with SqlAlchemy

---
+++ arrivals_api/app/features/users/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+
+from app.core.write_db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    full_name = Column(String, index=True)
